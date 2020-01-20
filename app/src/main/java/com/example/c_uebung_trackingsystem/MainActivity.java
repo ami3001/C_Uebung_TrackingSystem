4223545047d7bce0dae64792a9b543dc09bc5b51
Andreas Pilger <a.pilger@htl-grieskirchen.at>
2020-01-20 12:29:49 +0100

    Änderung durch Pilger

--- app/src/main/java/com/example/c_uebung_trackingsystem/MainActivity.java
+++ app/src/main/java/com/example/c_uebung_trackingsystem/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Kommentar Pilger
+        //Ende
         final ListView trackinglist = (ListView)findViewById(R.id.listView);
 
         
